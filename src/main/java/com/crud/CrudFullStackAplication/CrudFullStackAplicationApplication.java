3bc6dfd5ee99be1485117643870c6d0e03edd986
Xxarhabia <msarabiacarrascal@gmail.com>
2023-06-24 20:05:41 -0500

    primer commit

---
+++ src/main/java/com/crud/CrudFullStackAplication/CrudFullStackAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.crud.CrudFullStackAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudFullStackAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudFullStackAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/CrudFullStackAplication/CrudFullStackAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.CrudFullStackAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFullStackAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
